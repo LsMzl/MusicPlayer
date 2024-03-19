b4a59da4767d41ceb6644120e454b488e0a27343
LsMzl <153521189+LsMzl@users.noreply.github.com>
2024-03-19 09:15:52 +0100

    essai fetch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,38 @@
+// Données de la piste en cours
 const AUDIO = document.querySelector("audio");
 const TRACK = document.getElementById("track");
 const TRACK_TIME = document.getElementById("track-time");
 const CURRENT_TIME = document.getElementById("current-time");
 const SONG_TITLE = document.getElementById("song_title");
 const ARTIST_NAME = document.getElementById("artist_name");
+const SONG_PICTURE = document.getElementById("songPicture");
 const VOLUME = document.getElementById("volume");
-const VOLUME_ICON = document.getElementById("volume_icon");
-const VOLUME_OFF = document.getElementById("volume_off");
 
 /* Boutons de controle du lecteur */
 const PLAY_BTN = document.getElementById("play-btn");
 const PAUSE_BTN = document.getElementById("pause-btn");
-const SKIP_BTN_F = document.getElementById("skipF-btn");
-const SKIP_BTN_B = document.getElementById("skipB-btn");
+const NEXT_BTN = document.getElementById("skipF-btn");
+const PREVIOUS_BTN = document.getElementById("skipB-btn");
+const VOLUME_ICON = document.getElementById("volume_icon");
+const VOLUME_OFF = document.getElementById("volume_off");
 
 /* On récupère la durée de la chanson en cours */
 let audioDuration = AUDIO.duration;
 
 buildDuration(audioDuration);
 
-/* Bouton Play & Pause */
+/**
+ * Convertit la durée d'une chanson en minutes et secondes
+ * @param {*} duration
+ */
+function buildDuration(duration) {
+  let minutes = Math.floor(duration / 60);
+  let reste = duration % 60;
+  let seconds = Math.floor(reste);
+  seconds = String(seconds).padStart(2, 0);
+
+  return minutes + ":" + seconds;
+}
 
 // Play
 PLAY_BTN.addEventListener("click", () => {
@@ -29,7 +42,7 @@ PLAY_BTN.addEventListener("click", () => {
   AUDIO.volume = VOLUME.value;
 });
 
-//Pause
+// Pause
 PAUSE_BTN.addEventListener("click", () => {
   PLAY_BTN.style.display = "inline-block";
   PAUSE_BTN.style.display = "none";
@@ -65,22 +78,40 @@ VOLUME_OFF.addEventListener("click", function () {
   VOLUME_ICON.style.display = "inline-block";
 });
 
-/**
- * Convertit la durée d'une chanson en minutes et secondes
- * @param {*} duration
- */
-function buildDuration(duration) {
-  let minutes = Math.floor(duration / 60);
-  let reste = duration % 60;
-  let seconds = Math.floor(reste);
-  seconds = String(seconds).padStart(2, 0);
+// Bouton Next
+NEXT_BTN.addEventListener("click", function () {
+  setSongInfos(+1);
+});
 
-  return minutes + ":" + seconds;
+// Bouton Suivant
+PREVIOUS_BTN.addEventListener("click", function () {
+  setSongInfos(-1);
+});
+
+// Récupération et affichage des données de la musique
+
+function setSongInfos(index) {
+  fetch("./assets/playlist.json")
+    .then((res) => res.json())
+    .then((songsData) => (SONG_TITLE.innerHTML = songsData[index].title));
+
+  fetch("./assets/playlist.json")
+    .then((res) => res.json())
+    .then((songsData) => (ARTIST_NAME.innerHTML = songsData[index].artist));
+
+  fetch("./assets/playlist.json")
+    .then((res) => res.json())
+    .then((songsData) => (AUDIO.src = songsData[index].path));
+
+  fetch("./assets/playlist.json")
+    .then((res) => res.json())
+    .then((songsData) => (TRACK_TIME.textContent = songsData[index].duration));
+
+  fetch("./assets/playlist.json")
+    .then((res) => res.json())
+    .then((songsData) =>
+      SONG_PICTURE.setAttribute("src", songsData[index].img)
+    );
 }
 
-// Ajout des éléments dans le HTML
-TRACK_TIME.textContent = buildDuration(AUDIO.duration);
-
-fetch("./assets/playlist.json")
-  .then((res) => res.json())
-  .then((songsData) => (SONG_TITLE.innerHTML = songsData.song1.title));
+setSongInfos(0);
